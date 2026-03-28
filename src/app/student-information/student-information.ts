4d06441065b4a7002c6d405690f0e526adf9e080
Gaurav Kumar <gaurav3421@gmail.com>
2026-03-28 17:42:33 +0530

    HTTP CLIENT PROGRAMMING 1.1

--- src/app/student-information/student-information.ts
+++ src/app/student-information/student-information.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Import CommonModule for common directives
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms'; // Import NgForm for template-driven forms
+import { Commonservice } from '../projectservices/commonservice'; // Step 1- 
 
 @Component({
   selector: 'app-student-information',
@@ -9,19 +10,37 @@ import { NgForm } from '@angular/forms'; // Import NgForm for template-driven fo
   templateUrl: './student-information.html',
   styleUrl: './student-information.css',
 })
-export class StudentInformation {
+export class StudentInformation implements OnInit  {
 
   // Data model for two-way binding
   // student Object to hold form data
   studentData = {
     fullName: '',
     age: null as number | null,
-    gender: ''
+    gender: '',
+    country: ''
+
   };
   genders = ['Male', 'Female', 'Other'];
+  countries:any[]= [];
 
 
-  
+  constructor( private _commonService: Commonservice ){}; // Injecting Commonservice Step 2
+
+  ngOnInit(): void {
+  console.log('Initializing Student Information Component');
+    // Step 3 -Using the injected service to get country list
+    this._commonService.getCountryList().subscribe(
+     ( result:any )=>
+     {
+       this.countries=result;
+       console.log('Country List:', this.countries);
+     }
+
+    )
+      
+  }
+
   // Method to handle form submission
   onSubmitStudentInformation(form: NgForm) {
     //if (form.valid) {
